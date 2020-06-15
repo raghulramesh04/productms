560a11750fa2d58d60c15dc3414d2218fd721816
raghulramesh04 <raghulramesh04@gmail.com>
2020-06-15 12:54:28 +0530

    productms

---
+++ src/main/java/com/infosys/project1/product/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.infosys.project1.product.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.infosys.project1.product.entity.Product;
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+}
